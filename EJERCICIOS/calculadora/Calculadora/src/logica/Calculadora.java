566633496f16523680b4d78770f17fc10e9e5062
Codigo Margarita Maza <91554777+escuelaDeCodigoMargaritaMaza@users.noreply.github.com>
2023-07-14 13:35:47 -0600

    Add files via upload

---
+++ EJERCICIOS/calculadora/Calculadora/src/logica/Calculadora.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logica;
+
+import gui.Ventana;
+import gui.Ventana2;
+
+/**
+ *
+ * @author PILARES
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Ventana ventana1 = new Ventana();// TODO code application logic here
+        //ventana1.setVisible(true);
+        //ventana1.setLocationRelativeTo(null);
+        
+        Ventana2 ventana2=new Ventana2();
+        ventana2.setVisible(true);
+        ventana2.setLocationRelativeTo(null);
+    }
+    
+}
